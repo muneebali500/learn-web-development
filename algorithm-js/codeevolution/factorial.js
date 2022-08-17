83fe4e706bef48ad7798c670034ce7a6d2bfbbba
Muneeb Ali <72123497+muneebali500@users.noreply.github.com>
2022-08-17 11:41:30 +0500

    Add files via upload

---
+++ algorithm-js/codeevolution/factorial.js
@@ -0,0 +1,17 @@
+function factorial(n) {
+  let result = 1;
+
+  for (let i = 2; i <= n; i++) {
+    result = result * i;
+  }
+
+  return result;
+}
+
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(2)); // 2
+console.log(factorial(4)); // 24
+
+// Big-O = O(n)
+// that is linear; as the input size grows, loop grows
