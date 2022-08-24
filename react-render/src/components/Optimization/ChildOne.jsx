46f85b4d6e93250aa98aa4e4fe1a46c66dd7287f
Muneeb Ali <72123497+muneebali500@users.noreply.github.com>
2022-08-24 04:04:05 -0700

    Delete react-render directory

--- react-render/src/components/Optimization/ChildOne.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function ChildOne() {
-  console.log(`child one render`);
-
-  return <div>Child</div>;
-}
--- react-render/src/components/Optimization/ChildTwo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ChildTwo() {
-  console.log(`child two render`);
-
-  return <div>Child two</div>;
-}
-
-export const MemoizedChild = React.memo(ChildTwo);
