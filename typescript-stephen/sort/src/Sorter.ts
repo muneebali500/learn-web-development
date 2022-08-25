3481b6e782246fc8d5498b96e4630b5b9b853cc1
Muneeb Ali <72123497+muneebali500@users.noreply.github.com>
2022-08-25 10:41:32 -0700

    Add files via upload

--- typescript-stephen/sort/src/Sorter.ts
+++ typescript-stephen/sort/src/Sorter.ts
@@ -1,21 +1,23 @@
-import { NumbersCollection } from "./NumbersCollecction";
+// export interface Sortable {
+//   length: number;
+//   compare(leftIndex: number, rightIndex: number): boolean;
+//   swap(leftIndex: number, rightIndex: number): void;
+// }
 
-export interface Sortable {
-  length: number;
-  compare(leftIndex: number, rightIndex: number): boolean;
-  swap(leftIndex: number, rightIndex: number): void;
-}
+export abstract class Sorter {
+  // constructor(public collection: Sortable);
 
-export class Sorter {
-  constructor(public collection: Sortable) {}
+  abstract compare(leftIndex: number, rightIndex: number): boolean;
+  abstract swap(leftIndex: number, rightIndex: number): void;
+  abstract length: number;
 
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
--- typescript-stephen/sort/src/index.ts
+++ typescript-stephen/sort/src/index.ts
@@ -1,8 +1,22 @@
+// import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollecction";
-import { Sorter } from "./Sorter";
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList.ts";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-
+numbersCollection.sort();
 console.log(numbersCollection.collection);
+
+const charactersCollection = new CharactersCollection(`Xaayb`);
+charactersCollection.sort();
+
+console.log(charactersCollection.collection);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(17);
+
+linkedList.sort();
+linkedList.print();
