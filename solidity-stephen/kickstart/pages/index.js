c1300a292f0c944fefbd12da566823a023137c43
Muneeb Ali <72123497+muneebali500@users.noreply.github.com>
2022-08-30 07:48:04 -0700

    Add files via upload

--- solidity-stephen/kickstart/pages/index.js
+++ solidity-stephen/kickstart/pages/index.js
@@ -1,7 +1,27 @@
+import "semantic-ui-css/semantic.min.css";
+
 import factory from "../ethereum/factory";
+import { Button, Card } from "semantic-ui-react";
 
 export default function Home({ campaigns }) {
-  return <div>{campaigns[0]}</div>;
+  function renderCampaigns() {
+    const items = campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true,
+      };
+    });
+
+    return <Card.Group items={items} />;
+  }
+
+  return (
+    <div>
+      <div>{renderCampaigns()}</div>
+      <Button content="Create Campaign" icon="add circle" primary />
+    </div>
+  );
 }
 
 Home.getInitialProps = async () => {
