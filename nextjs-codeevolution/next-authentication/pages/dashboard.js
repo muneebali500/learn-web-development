99bdaf02d64251dd3e2b96660037d133e0d97ec1
Muneeb Ali <72123497+muneebali500@users.noreply.github.com>
2022-09-01 11:09:40 -0700

    Add files via upload

--- nextjs-codeevolution/next-authentication/pages/dashboard.js
+++ nextjs-codeevolution/next-authentication/pages/dashboard.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getSession, signIn } from "next-auth/client";
 
 export default function Dashboard() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function securePage() {
+      const session = await getSession();
+
+      if (!session) {
+        signIn();
+      } else {
+        setLoading(false);
+      }
+    }
+    securePage();
+  }, []);
+
+  if (loading) {
+    return <h2>loading...</h2>;
+  }
+
   return <div>dashboard</div>;
 }
