0e0613b8b51c3a8a0615bf4565d287679e1f21c7
Muneeb Ali <72123497+muneebali500@users.noreply.github.com>
2022-09-02 08:00:13 -0700

    Add files via upload

--- nextjs-codeevolution/next-authentication/pages/api/test-session.js
+++ nextjs-codeevolution/next-authentication/pages/api/test-session.js
@@ -1,10 +1,11 @@
-// import { getSession } from 'next-auth/client'
+import { getSession } from "next-auth/client";
 
-// export default async (req, res) => {
-//   const session = await getSession({ req })
-//   if (!session) {
-//     res.status(401).json({ error: 'Unauthenticated user' })
-//   } else {
-//     res.status(200).json({ message: 'Success', session })
-//   }
-// }
+export default async function (req, res) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    res.status(401).json({ error: `unauthorized access` });
+  } else {
+    res.status(200).json({ message: `success`, session });
+  }
+}
--- nextjs-codeevolution/next-authentication/pages/blog.js
+++ nextjs-codeevolution/next-authentication/pages/blog.js
@@ -1,5 +1,27 @@
-import React from "react";
+import { getSession, useSession } from "next-auth/client";
 
-export default function Bblog() {
-  return <div>blog</div>;
+export default function Bblog({ data }) {
+  return <div>blog - {data}</div>;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: `/api/auth/signin?callbackUrl=http://localhost:3000/blog`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+      data: session
+        ? `list of personalized blogs`
+        : `list of 100 personalized blogs`,
+    },
+  };
 }
