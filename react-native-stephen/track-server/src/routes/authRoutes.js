25b1eb5bc208d5bbb5b12b952508cd4b5227d7b4
Muneeb Ali <72123497+muneebali500@users.noreply.github.com>
2022-09-08 05:40:09 -0700

    Add files via upload

--- react-native-stephen/track-server/src/routes/authRoutes.js
+++ react-native-stephen/track-server/src/routes/authRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 const router = express.Router();
@@ -6,9 +7,21 @@ const router = express.Router();
 router.post(`/signup`, async (req, res) => {
   const { email, password } = req.body;
 
-  const user = new User({ email, password });
-  await user.save();
-  res.status(201).json({ success: true, user });
+  try {
+    const user = new User({ email, password });
+    await user.save();
+
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+
+    res.status(201).send(token);
+  } catch (err) {
+    return res.status(422).json({ success: "fail", msg: err.message });
+  }
+});
+
+router.get(`/signup`, async (req, res) => {
+  const users = await User.find();
+  res.status(200).json({ success: true, users });
 });
 
 export default router;
