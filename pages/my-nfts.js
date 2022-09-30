f78f32f4e8581f8530156901db5c33e4be80d7f3
Muneeb Ali <72123497+muneebali500@users.noreply.github.com>
2022-09-30 14:53:28 +0500

    Delete pages directory

--- pages/my-nfts.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const MyNFTs = () => <div>MyNFTs</div>;
-
-export default MyNFTs;
--- pages/nft-details.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const NFTDetails = () => <div>NFTDetails</div>;
-
-export default NFTDetails;
